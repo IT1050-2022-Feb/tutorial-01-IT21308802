f48e6bd7d063a58012f6855728402e9f3f62b569
IT21308802 <>
2022-03-02 12:06:18 +0000

    exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+int marks1, marks2, total=0 ;
+float average ;
+
+  printf("Enter marks for the Subject 1 : ");
+  scanf("%d",&marks1);
+
+  printf("Enter marks for the Subject 2 : ");
+  scanf("%d",&marks2);
+
+  total =marks1+marks2;
+  average=total/2;
+
+  printf("Average mark is %.2f",average);
   
   return 0;
 }
